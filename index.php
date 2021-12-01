bde8d5872831b3e7eafc15256026dddca5c0f89e
Paweł Kellner <karkuwa@Pawel-MacBook.local>
2021-12-01 14:49:14 +0100

    stap4 to 6 af

--- index.php
+++ index.php
@@ -9,6 +9,26 @@
     <?php wp_head(); ?>
 </head>
 <body>
-    <p>Hallo wereld!</p>
+<?php
+get_header();
+?>
+
+    <div class="Contenplek">
+        <?php
+        if (have_posts()):
+            while (have_posts()) : the_post(); ?>
+        <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+        <?php the_content() ?>
+
+        <?php endwhile;
+        else :
+            echo '<p>There are no posts!</p>';
+        endif;
+        ?>
+    </div>
+
+<?php
+get_footer();
+?>
 </body>
 </html>
